0ef95ff928b5cc0a9012fef1e39b8a66cb139a2b
will <will.j.nagel@gmail.com>
2024-05-07 08:18:00 -0500

    Added images to recycler view cards

--- app/src/main/java/com/example/pickup/GameAdapter.kt
+++ app/src/main/java/com/example/pickup/GameAdapter.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
@@ -15,24 +16,44 @@ class GameAdapter(private val games: List<Map<String, Any>>) : RecyclerView.Adap
     inner class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val sportTextView: TextView = itemView.findViewById(R.id.sportTextView)
         private val locationTextView: TextView = itemView.findViewById(R.id.locationTextView)
-        private val minPlayersTextView: TextView = itemView.findViewById(R.id.minPlayersTextView)
-        private val maxPlayersTextView: TextView = itemView.findViewById(R.id.maxPlayersTextView)
+        //private val minPlayersTextView: TextView = itemView.findViewById(R.id.minPlayersTextView)
+       // private val maxPlayersTextView: TextView = itemView.findViewById(R.id.maxPlayersTextView)
         private val dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
         private val timeTextView: TextView = itemView.findViewById(R.id.timeTextView)
-        private val teamTextView: TextView = itemView.findViewById(R.id.teamTextView)
+        private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        //private val teamTextView: TextView = itemView.findViewById(R.id.teamTextView)
         private val cardView: CardView = itemView.findViewById(R.id.cardView)
 
 
+        private val imageView: ImageView = itemView.findViewById(R.id.sportImage)
+
 
         fun bind(game: Map<String, Any>) {
-            sportTextView.text = game["sport"].toString()
+
+            val sport = game["sport"].toString()
+
+            if (sport == "Soccer"){
+                imageView.setImageResource(R.drawable.soccer_ball)
+            }
+            if (sport == "Basketball"){
+                imageView.setImageResource(R.drawable.basketball)
+            }
+            if (sport == "Volleyball"){
+                imageView.setImageResource(R.drawable.volleyball)
+            }
+            if (sport == "Pickle ball"){
+                imageView.setImageResource(R.drawable.app_logo__4_)
+            }
+
+            sportTextView.text = sport
 
             locationTextView.text = game["location"].toString()
-            minPlayersTextView.text = game["minPlayers"].toString()
-            maxPlayersTextView.text = game["maxPlayers"].toString()
+           // minPlayersTextView.text = game["minPlayers"].toString()
+           // maxPlayersTextView.text = game["maxPlayers"].toString()
+            titleTextView.text = game["title"].toString()
             dateTextView.text = game["date"].toString()
             timeTextView.text = game["time"].toString()
-            teamTextView.text = game["team"].toString()
+            //teamTextView.text = game["team"].toString()
         }
     }
 
